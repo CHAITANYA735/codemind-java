a546456e7feec6433185678b8042f341c61d79be
CHAITANYA735 <97425744+CHAITANYA735@users.noreply.github.com>
2024-03-02 22:41:59 +0530

    import java.util.*; public class mai{     public static void main(String args[]){         Scanner ip=new Scanner(System.in);         char k=ip.next().charAt(0);         int m=k;         System.out.println(m);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class mai{
+    public static void main(String args[]){
+        Scanner ip=new Scanner(System.in);
+        char k=ip.next().charAt(0);
+        int m=k;
+        System.out.println(m);
+    }
+}
