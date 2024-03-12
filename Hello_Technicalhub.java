b43d5a0b0263f04177544c16abffdcc467b585bb
CHAITANYA735 <97425744+CHAITANYA735@users.noreply.github.com>
2024-03-12 08:56:12 +0530

    import java.util.*; public class min{     public static void main(String a[]){         Scanner ip=new Scanner(System.in);         String s=ip.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class min{
+    public static void main(String a[]){
+        Scanner ip=new Scanner(System.in);
+        String s=ip.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s);
+    }
+}
